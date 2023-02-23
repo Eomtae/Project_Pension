85ddef42aaebc5795f333578c1229ea7d342a2a1
Eomtae <theum0723@gmail.com>
2023-02-23 18:09:10 +0900

    2023-02-23

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
 
-            var swiper = new Swiper(".mySwiper", {
-                slidesPerView: 1,
-                spaceBetween: 30,
-                effect:"fade",
-                loop: true,
-                autoplay:{
-                  delay:4000,
-                },
-                pagination: {
-                  clickable: true,
-                },
-                navigation: {
-                  nextEl: ".swiper-button-next",
-                  prevEl: ".swiper-button-prev",
-                },
-              });//스와이퍼 JS입니다.
-
