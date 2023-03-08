75367ccdbfdcf2f0abbdddd4ca2fef99b0ac9fb7
Eomtae <theum0723@gmail.com>
2023-03-08 18:20:13 +0900

    2023-03-08

--- js/index.js
+++ js/index.js
@@ -1,53 +1,43 @@
 
 //스크롤 이벤트
 const elFirst = document.querySelector('.first')
+const elSecond = document.querySelector('.second-text')
+const elFlex = document.querySelector('.flexbox')
+const elThird = document.querySelector('.third-text')
+const elMap = document.querySelector('#map')
+const elThirdImg = document.querySelector('.third-imgbox')
+
 window.addEventListener('scroll',function(){ 
   if( 
     elFirst.offsetTop - window.innerHeight * 0.8 < window.pageYOffset ){
       elFirst.classList.add('active')
     } 
-})
 
-const elSecond = document.querySelector('.second-text')
-window.addEventListener('scroll',function(){ 
   if( 
     elSecond.offsetTop - window.innerHeight * 0.8 < window.pageYOffset ){
       elSecond.classList.add('active')
     } 
-})
 
-const elFlex = document.querySelector('.flexbox')
-window.addEventListener('scroll',function(){ 
   if( 
     elFlex.offsetTop - window.innerHeight * 0.8 < window.pageYOffset ){
       elFlex.classList.add('active')
     } 
-})
 
-const elThird = document.querySelector('.third-text')
-window.addEventListener('scroll',function(){ 
   if( 
-    elThird.offsetTop - window.innerHeight * 0.8 < window.pageYOffset ){
+    elThird.offsetTop - window.innerHeight * 0.8< window.pageYOffset ){
       elThird.classList.add('active')
     } 
-})
 
-const elMap = document.querySelector('#map')
-window.addEventListener('scroll',function(){
   if(
     elMap.offsetTop - this.window.innerHeight * 0.8 < this.window.pageYOffset){
       elMap.classList.add('active')
   }
-})
 
-const elThirdImg = document.querySelector('.third-imgbox')
-window.addEventListener('scroll',function(){
   if(
     elThirdImg.offsetTop - this.window.innerHeight * 0.8 < this.window.pageYOffset){
       elThirdImg.classList.add('active')
   }
 })
-console.log('hgh')
 
 // 지도 API
 		var container = document.getElementById('map');
